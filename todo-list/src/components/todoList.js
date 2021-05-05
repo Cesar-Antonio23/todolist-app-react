3e62c3cc1869bebf232786c8cd82ef6b55be46c0
Cesar-Acevedo23 <ces.acevedo.23@gmail.com>
2021-05-05 19:40:23 -0400

    Agregados eliminar y editar, falta aplicar useEffect

--- todo-list/src/components/todoList.js
+++ todo-list/src/components/todoList.js
@@ -13,6 +13,20 @@ function TodoList() {
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos);
+    };
+
+    const updateTodo = (todoId, newValue) =>{
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return 
+        }
+
+        setTodos(prev => prev.map(item =>(item.map === todoId ? newValue : item)))
+    }
+
+    const removeTodo= id =>{
+        const removeArr = [...todos].filter(todo=> todo.id !==id)
+        
+        setTodos(removeArr);
     }
 
     const completeTodo = id =>{
@@ -29,7 +43,9 @@ function TodoList() {
         <div>
             <h1>Cual es el plan para hoy?</h1>
             <TodoForm onSubmit={addTodo}/>
-            <Todo todos={todos} completeTodo={completeTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            updateTodo={updateTodo}
+            />
         </div>
     )
 }
