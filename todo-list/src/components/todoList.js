5e577c84ccda9c85a0cfd491131068c28af307be
Cesar-Acevedo23 <ces.acevedo.23@gmail.com>
2021-05-05 19:17:36 -0400

    Falta aplicar el close

--- todo-list/src/components/todoList.js
+++ todo-list/src/components/todoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './todoForm'
+import Todo from './todo'
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,10 +15,21 @@ function TodoList() {
         setTodos(newTodos);
     }
 
+    const completeTodo = id =>{
+        let updateTodos = todos.map(todo =>{
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        })
+        setTodos(updateTodos);
+    }
+
     return (
         <div>
             <h1>Cual es el plan para hoy?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo}/>
         </div>
     )
 }
